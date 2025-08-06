8fddcb9d070821ed50e6d7ec2aa1f2cfde439d6e
Vallor23 <enosnyabinda2017@gmail.com>
2025-08-06 07:46:08 +0300

    update product model

--- shopsy/products/models.py
+++ shopsy/products/models.py
@@ -1,10 +1,22 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 class Product(models.Model):
+    CATEGORY = (
+        ( "electronics", "Electronics"),
+        ("clothings", "Clothings"),
+        ("groceries", "Groceries")
+    )
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    img = models.ImageField(upload_to="img")
+    slug = models.SlugField(unique=True, blank=True, null=True)
     stock = models.IntegerField()
+    category = models.CharField(max_length=15, choices=CATEGORY, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug
 
     def __str__(self):
-        return self.name
+        return f"{self.name} ({self.category})"
