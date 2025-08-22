ca2a4bf68350fc1b6cb052d03bafb26f08fd498a
Vallor23 <enosnyabinda2017@gmail.com>
2025-08-22 19:19:41 +0300

    feat(serializers): add UserSerializer with custom user creation logic

---
+++ shopsy/users/serializers.py
@@ -0,0 +1,21 @@
+from .models import User
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'phone']
+        read_only_fields = ['id', 'date_joined']
+        extra_kwargs={
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        user =  User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            # phone=validated_data
+        )
+        return user
