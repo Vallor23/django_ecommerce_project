ce724a18311af1b6cbefa0d1bfbf40b1f8105558
Vallor23 <enosnyabinda2017@gmail.com>
2025-08-22 19:24:53 +0300

    feat(auth): add user registration and login endpoints with JWT authentication

--- shopsy/users/views.py
+++ shopsy/users/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from .serializers import UserSerializer
+from django.contrib.auth import authenticate, login
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+@api_view(['POST'])
+def register_user(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        refresh = RefreshToken.for_user(user)
+
+        return Response({
+            'message': 'User registered successfully',
+            'user': serializer.data,
+            'tokens': {
+                'refresh': str(refresh),
+                'access': str(refresh.access_token)
+            }
+        },status=status.HTTP_201_CREATED)
+    return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def login_user(request):
+    username = request.data.get('username')
+    password= request.data.get('password')
+
+    if not username or not password:
+        return Response({'error': 'Please provide both username and password'}, status=status.HTTP_400_BAD_REQUEST)
+
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            'message': 'Login sucessful',
+            'username': user.username,
+            'tokens': {
+                'refresh': str(refresh),
+                'access': str(refresh.access_token)
+            }
+        }, status=status.HTTP_200_OK)
+    return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+    
