3622e07f7634636d553caa45a9f5dbf09afdfa95
Vallor23 <enosnyabinda2017@gmail.com>
2025-08-27 23:16:29 +0300

    chore: initialize project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 import Home from "./pages/Home"
-// import Login from './pages/Login'
-// import Register from './pages/Register'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import { Toaster } from "@/components/ui/sonner"
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} /> */}
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+      <Toaster richColors position="top-right"/>
+    </>
   )
 }
 
