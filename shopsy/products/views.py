eceabac4d5098034564f7cf55f7f764fe2a1c115
Vallor23 <enosnyabinda2017@gmail.com>
2025-09-02 00:58:43 +0300

    feat(settings): add product endpoints, set filter backend for search and ordering and custom viwe for listing featured products

--- shopsy/products/views.py
+++ shopsy/products/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Product
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from .serializers import ProductDetailSerializer, ProductListSerializer
 
 # Create your views here.
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return ProductListSerializer
+        return ProductDetailSerializer
+
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    lookup_field = 'slug'
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['category', 'price', 'is_featured']
+    search_fields = ['name', 'category']
+    ordering_fields = ['price', 'created_at']
+
+    @action(detail=False, methods=['get'])
+    def featured(self, request):
+        featured_products = self.get_queryset().filter(is_featured=True)
+        serializer = self.get_serializer(featured_products, many=True)
+        return Response(serializer.data)
