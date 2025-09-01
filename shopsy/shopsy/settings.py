1de875cf2dc5358e4b37be617411d57557b4f594
Vallor23 <enosnyabinda2017@gmail.com>
2025-09-01 23:53:10 +0300

    feat(settings): configure DjangoFilterBackend

--- shopsy/shopsy/settings.py
+++ shopsy/shopsy/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'products',
     "corsheaders",
     'rest_framework',
-     'rest_framework_simplejwt',
+    'rest_framework_simplejwt',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -134,7 +135,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
 
